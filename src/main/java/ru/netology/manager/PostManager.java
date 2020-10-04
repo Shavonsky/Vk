0a98cbed7c9af7835f96d0905434ab6f5120317b
Ekaterina Popova <katerina.shavonsky@yandex.ru>
2020-10-04 16:57:57 +0300

    Manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+    public Post[] search(int ownerID, String domain, String query, boolean ownersOnly, byte count, int offset) {
+        return null;
+    }
+
+    public Post[] delete(int ownerID, int postID){
+        return null;
+    }
+}

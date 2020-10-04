832e23643f63a95299f974ba4832e97785bb1071
Ekaterina Popova <katerina.shavonsky@yandex.ru>
2020-10-04 14:49:27 +0300

    add

---
+++ src/main/java/ru/netology/domain/geo.java
@@ -0,0 +1,31 @@
+package ru.netology.domain;
+
+public class geo {
+    String type;
+    String coordinates;
+    private int object;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(String coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public int getObject() {
+        return object;
+    }
+
+    public void setObject(int object) {
+        this.object = object;
+    }
+}
